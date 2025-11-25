6008bb7a376a0ca86eaa31fde4c9cfbc1ff19e1d
Adrian Claros <166774927+AdrianCV2345@users.noreply.github.com>
2025-11-25 13:10:45 -0500

    Feat: All functions of SongService has been created.

--- Services/ISongService.cs
+++ Services/ISongService.cs
@@ -1,6 +1,14 @@
-﻿namespace ProyectoTecWeb.Services
+﻿using ProyectoTecWeb.Models;
+using ProyectoTecWeb.Models.DTOS;
+
+namespace ProyectoTecWeb.Services
 {
     public interface ISongService
     {
+        Task<IEnumerable<Song>> GetAll();
+        Task<Song> GetOne(Guid id);
+        Task<Song> CreateSong(CreateSongDto dto);
+        Task<Song> UpdateSong(UpdateSongDto dto, Guid id);
+        Task DeleteSong(Guid id);
     }
 }
--- Services/SongService.cs
+++ Services/SongService.cs
@@ -1,6 +1,56 @@
-﻿namespace ProyectoTecWeb.Services
+﻿using ProyectoTecWeb.Models;
+using ProyectoTecWeb.Models.DTOS;
+using ProyectoTecWeb.Repositories;
+
+namespace ProyectoTecWeb.Services
 {
-    public class SongService
+    public class SongService : ISongService
     {
+        private readonly ISongRepository _repo;
+        public SongService(ISongRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<Song> CreateSong(CreateSongDto dto)
+        {
+
+            var song = new Song
+            {
+                Id = Guid.NewGuid(),
+                Name = dto.Name,
+                Description = dto.Description,
+                duration = dto.duration
+            };
+            await _repo.Add(song);
+            return song;
+        }
+
+        public Task DeleteSong(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Song>> GetAll()
+        {
+            return await _repo.GetAll();
+        }
+
+        public async Task<Song> GetOne(Guid id)
+        {
+            return await _repo.GetOne(id);
+        }
+
+        public async Task<Song> UpdateSong(UpdateSongDto dto, Guid id)
+        {
+            Song? song = await GetOne(id);
+            if (song == null) throw new Exception("Song doesnt exist.");
+
+            song.Name = dto.Name;
+            song.Description = dto.Description; 
+            song.duration = dto.duration;
+
+            await _repo.Update(song);
+            return song;
+        }
     }
 }
