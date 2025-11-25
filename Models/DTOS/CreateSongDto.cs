6e9e8f63b79c5830c1abb31f9fe3d80205f19653
Adrian Claros <166774927+AdrianCV2345@users.noreply.github.com>
2025-11-25 13:09:58 -0500

    feat: Dtos for Song has been created.

--- Models/DTOS/CreateSongDto.cs
+++ Models/DTOS/CreateSongDto.cs
@@ -1,6 +1,19 @@
-﻿namespace ProyectoTecWeb.Models.DTOS
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoTecWeb.Models.DTOS
 {
     public class CreateSongDto
     {
+        
+        [Required, StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required, Range(0, 60)]
+        public float duration { get; set; }
+
+
     }
 }
--- Models/DTOS/UpdateSongDto.cs
+++ Models/DTOS/UpdateSongDto.cs
@@ -1,6 +1,14 @@
-﻿namespace ProyectoTecWeb.Models.DTOS
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoTecWeb.Models.DTOS
 {
     public class UpdateSongDto
     {
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public float duration { get; set; }
+
     }
 }
