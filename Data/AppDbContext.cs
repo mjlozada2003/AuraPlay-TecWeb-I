2ef6477427ab49d63a813886f109b3723e110e75
Maria <mariajesuslozada10@gmail.com>
2025-11-26 20:59:29 -0400

    fixing stadistics

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,21 +1,42 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProyectoTecWeb.Models;
 using System.Collections.Generic;
-using System.Xml.Linq;
+using System.Reflection.Emit;
 
 namespace ProyectoTecWeb.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
         public DbSet<Song> Songs => Set<Song>();
+        public DbSet<Statistics> Statistics => Set<Statistics>();
+        public DbSet<Playlist> Playlists => Set<Playlist>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-       
+
+            // Relación 1:1 (Song - Statistics)
+            modelBuilder.Entity<Song>()
+                .HasOne(s => s.Statistics)
+                .WithOne(st => st.Song)
+                .HasForeignKey<Statistics>(st => st.SongId)
+                .OnDelete(DeleteBehavior.Cascade); 
+
+            // Relación 1:N (User - Playlist)
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Playlists)
+                .WithOne(p => p.User)
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Relación N:M (Song - Playlist)
+            modelBuilder.Entity<Playlist>()
+                .HasMany(p => p.Songs)
+                .WithMany(s => s.Playlists)
+                .UsingEntity(j => j.ToTable("PlaylistSongs")); 
         }
     }
 }
