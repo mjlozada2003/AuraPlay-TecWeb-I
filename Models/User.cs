6c71d77a970e492775652c2f4778f69ef1f25417
Adrian Claros <166774927+AdrianCV2345@users.noreply.github.com>
2025-11-27 14:39:10 -0500

    feat:Model for user has been created.

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoTecWeb.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        [Required, StringLength(128)]
+        public string Username { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+        public string Role { get; set; } = "User";//"User" | "Admin"
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiresAt { get; set; }
+        public DateTime? RefreshTokenRevokedAt { get; set; }
+        public string? CurrentJwtId { get; set; }
+    }
+}
